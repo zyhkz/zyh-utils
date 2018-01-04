d266dbcf423657b49e1aa354f5ed759f3e284f60
Yuan Hang Zhao <shyhzhao@cn.ibm.com>
2018-01-04 14:47:01 +0800

    optimize the code

--- MakeUpdate/MakeUpdate.py
+++ MakeUpdate/MakeUpdate.py
@@ -9,20 +9,25 @@ from tkinter import messagebox
 from tkinter import *
 
 
+def getPath(path, oldFolder, newFolder, targetFolder):
+    path = os.path.relpath(path, newFolder)
+    oldFile = os.path.join(oldFolder, path)
+    newFile = os.path.join(newFolder, path)
+    targetFile = os.path.join(targetFolder, path)
+    return (path, oldFile, newFile, targetFile)
+
+
 def makeUpdate(oldFolder, newFolder, targetFolder):
     for root, dirs, files in os.walk(newFolder):
         for folder in dirs:
-            path = os.path.relpath(os.path.join(root, folder), newFolder)
-            oldFile = os.path.join(oldFolder, path)
-            targetFile = os.path.join(targetFolder, path)
+            path, oldFile, newFile, targetFile = getPath(os.path.join(
+                root, folder), oldFolder, newFolder, targetFolder)
             if(not os.path.exists(oldFile) and not os.path.exists(targetFile)):
                 print("Create folder: " + path)
                 os.makedirs(targetFile)
         for file in files:
-            path = os.path.relpath(os.path.join(root, file), newFolder)
-            oldFile = os.path.join(oldFolder, path)
-            newFile = os.path.join(newFolder, path)
-            targetFile = os.path.join(targetFolder, path)
+            path, oldFile, newFile, targetFile = getPath(os.path.join(
+                root, file), oldFolder, newFolder, targetFolder)
             if (not os.path.exists(oldFile) or not filecmp.cmp(newFile, oldFile)):
                 if (not os.path.exists(os.path.dirname(targetFile))):
                     os.makedirs(os.path.dirname(targetFile))
